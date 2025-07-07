dae80f3154c01d443ce7631977c272f982ba52d2
Cassia Ang <cassiaang2006@gmail.com>
2025-07-07 13:42:36 +0800

    fix: weather api decoder

--- src/WeatherHelper.h
+++ src/WeatherHelper.h
@@ -70,24 +70,68 @@ private:
         http.end();
     }
 
-    static const char *decodeWeather(int code) {
-        if (code == 0)
-            return "Clear";
-        if (code < 3)
+    const char *decodeWeather(int code) {
+        switch (code) {
+        case 0:
+            return "Clear sky";
+        case 1:
+            return "Mainly clear";
+        case 2:
             return "Partly cloudy";
-        if (code < 45)
-            return "Cloudy";
-        if (code < 57)
+        case 3:
+            return "Overcast";
+        case 45:
             return "Fog";
-        if (code < 67)
-            return "Drizzle";
-        if (code < 77)
-            return "Rain";
-        if (code < 87)
-            return "Snow";
-        return "Thunder";
+        case 48:
+            return "Depositing rime fog";
+        case 51:
+            return "Drizzle: Light";
+        case 53:
+            return "Drizzle: Moderate";
+        case 55:
+            return "Drizzle: Dense";
+        case 56:
+            return "Freezing Drizzle: Light";
+        case 57:
+            return "Freezing Drizzle: Dense";
+        case 61:
+            return "Rain: Slight";
+        case 63:
+            return "Rain: Moderate";
+        case 65:
+            return "Rain: Heavy";
+        case 66:
+            return "Freezing Rain: Light";
+        case 67:
+            return "Freezing Rain: Heavy";
+        case 71:
+            return "Snowfall: Slight";
+        case 73:
+            return "Snowfall: Moderate";
+        case 75:
+            return "Snowfall: Heavy";
+        case 77:
+            return "Snow grains";
+        case 80:
+            return "Rain showers: Slight";
+        case 81:
+            return "Rain showers: Moderate";
+        case 82:
+            return "Rain showers: Violent";
+        case 85:
+            return "Snow showers: Slight";
+        case 86:
+            return "Snow showers: Heavy";
+        case 95:
+            return "Thunderstorm: Slight";
+        case 96:
+            return "Thunderstorm with slight hail";
+        case 99:
+            return "Thunderstorm with heavy hail";
+        default:
+            return "Unknown";
+        }
     }
-
     DisplayManager &_disp;
     uint32_t _lastFetch = 0;
     float _currentTemp = 0.0;
